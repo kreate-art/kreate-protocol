1480f8cdda794ba9bf8683c988c08beb3be027c5
sk-saru <saru@shinka.network>
2022-12-31 01:54:44 +0700

    test: bootstrap protocol tx using lucid emulator

---
+++ src/commands/gen-protocol-params.ts
@@ -0,0 +1,194 @@
+import { Lucid, UTxO } from "lucid-cardano";
+
+import { exportScript } from "@/contracts/compile";
+import { MigratableScript, Registry } from "@/schema/teiki/protocol";
+import { Hex } from "@/types";
+
+import {
+  constructMigratableScript,
+  constructScriptHash,
+} from "../transactions/helpers/constructors";
+
+import {
+  compileBackingVScript,
+  compileDedicatedTreasuryVScript,
+  compileOpenTreasuryVScript,
+  compileProjectDetailVScript,
+  compileProjectScriptVScript,
+  compileProjectVScript,
+  compileProjectsAtScript,
+  compileProofOfBackingMpScript,
+  compileProtocolNftMpScript,
+  compileProtocolSvScript,
+  compileSharedTreasuryVScript,
+  compileTeikiMpScript,
+} from "./compile-scripts";
+
+export type RegistryScript = {
+  protocolStakeValidatorHash: Hex;
+  projectValidatorHash: Hex;
+  projectDetailValidatorHash: Hex;
+  projectScriptValidatorHash: Hex;
+  backingValidatorHash: Hex;
+  dedicatedTreasuryValidatorHash: Hex;
+  sharedTreasuryValidatorHash: Hex;
+  openTreasuryValidatorHash: Hex;
+};
+
+export function getProtocolRegistryScript(
+  lucid: Lucid,
+  seedUtxo: UTxO,
+  teikiPlantNftMph: Hex
+): RegistryScript {
+  const protocolNftPolicy = exportScript(compileProtocolNftMpScript(seedUtxo));
+
+  const protocolNftMph = lucid.utils.validatorToScriptHash(protocolNftPolicy);
+
+  const projectsAuthTokenMph = lucid.utils.validatorToScriptHash(
+    exportScript(compileProjectsAtScript(protocolNftMph))
+  );
+
+  const teikiMPH = lucid.utils.validatorToScriptHash(
+    exportScript(compileTeikiMpScript(teikiPlantNftMph))
+  );
+
+  const proofOfBackingMph = lucid.utils.validatorToScriptHash(
+    exportScript(
+      compileProofOfBackingMpScript(
+        projectsAuthTokenMph,
+        protocolNftMph,
+        teikiMPH
+      )
+    )
+  );
+
+  return {
+    protocolStakeValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(compileProtocolSvScript(protocolNftMph))
+    ),
+    projectValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(compileProjectVScript(projectsAuthTokenMph, protocolNftMph))
+    ),
+    projectDetailValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(
+        compileProjectDetailVScript(projectsAuthTokenMph, protocolNftMph)
+      )
+    ),
+    projectScriptValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(
+        compileProjectScriptVScript(projectsAuthTokenMph, protocolNftMph)
+      )
+    ),
+    backingValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(compileBackingVScript(proofOfBackingMph, protocolNftMph))
+    ),
+    dedicatedTreasuryValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(
+        compileDedicatedTreasuryVScript(projectsAuthTokenMph, protocolNftMph)
+      )
+    ),
+    sharedTreasuryValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(compileSharedTreasuryVScript(protocolNftMph))
+    ),
+    openTreasuryValidatorHash: lucid.utils.validatorToScriptHash(
+      exportScript(
+        compileOpenTreasuryVScript(
+          projectsAuthTokenMph,
+          protocolNftMph,
+          teikiMPH
+        )
+      )
+    ),
+  };
+}
+
+export function getMigratableScript(
+  validatorHash: Hex,
+  migrateTokenMph: Hex,
+  migrateTokenName: string
+): MigratableScript {
+  return constructMigratableScript(validatorHash, {
+    [validatorHash]: {
+      mintingPolicyHash: migrateTokenMph,
+      tokenName: migrateTokenName,
+    },
+  });
+}
+
+export function getProtocolRegistry(
+  lucid: Lucid,
+  seedUtxo: UTxO,
+  teikiPlantNftMph: Hex,
+  migrateTokenMph: Hex,
+  migrateTokenName: string
+): Registry {
+  const registryScript: RegistryScript = getProtocolRegistryScript(
+    lucid,
+    seedUtxo,
+    teikiPlantNftMph
+  );
+
+  return {
+    protocolStakingValidator: constructScriptHash(
+      registryScript.protocolStakeValidatorHash
+    ),
+    projectValidator: getMigratableScript(
+      registryScript.projectValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+    projectDetailValidator: getMigratableScript(
+      registryScript.projectDetailValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+    projectScriptValidator: getMigratableScript(
+      registryScript.projectScriptValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+    backingValidator: getMigratableScript(
+      registryScript.backingValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+    dedicatedTreasuryValidator: getMigratableScript(
+      registryScript.dedicatedTreasuryValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+    sharedTreasuryValidator: getMigratableScript(
+      registryScript.sharedTreasuryValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+    openTreasuryValidator: getMigratableScript(
+      registryScript.openTreasuryValidatorHash,
+      migrateTokenMph,
+      migrateTokenName
+    ),
+  };
+}
+
+// TODO: sk-saru should read from config file
+export function getProtocolContants() {
+  return {
+    governorShareRatio: 600_000n,
+    protocolFundsShareRatio: 600_000n,
+    discountCentPrice: 10_000n,
+    projectMilestones: [1_000_000n, 10_000_000n, 100_000_000n],
+    teikiCoefficient: 500n,
+    projectTeikiBurnRate: 600_000n,
+    epochLength: { milliseconds: 10_000n },
+    projectPledge: 50_000_000n,
+    projectCreationFee: 20_000_000n,
+    projectSponsorshipFee: 10_000_000n,
+    projectSponsorshipDuration: { milliseconds: 10_000n },
+    projectInformationUpdateFee: 10_000_000n,
+    projectCommunityUpdateFee: 10_000_000n,
+    minTreasuryPerMilestoneEvent: 20_000_000n,
+    stakeKeyDeposit: 30_000_000n,
+    proposalWaitingPeriod: { milliseconds: 10_000n },
+    projectDelistWaitingPeriod: { milliseconds: 10_000n },
+  };
+}
