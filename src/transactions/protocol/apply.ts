e26dbc18c101475205bb1a716a8c30b38288bc7e
Umiuma <umiuma@shinka.network>
2022-12-30 16:14:34 +0700

    fix: complete the apply protocol proposal tx

--- src/transactions/protocol/apply.ts
+++ src/transactions/protocol/apply.ts
@@ -1,46 +1,80 @@
-import { Data, Lucid, Redeemer, UTxO } from "lucid-cardano";
+import { Lucid, toHex, UTxO } from "lucid-cardano";
 
-import { ProtocolParamsDatum } from "@/schema/teiki/protocol";
+import * as S from "@/schema";
+import {
+  ProtocolParamsDatum,
+  ProtocolParamsRedeemer,
+  ProtocolProposalDatum,
+  ProtocolProposalRedeemer,
+} from "@/schema/teiki/protocol";
+import { TimeDifference } from "@/types";
+import { assert } from "@/utils";
 
-export type ApplyTxParams = {
-  protocolParamsDatum: ProtocolParamsDatum;
+export type ApplyProtocolTxParams = {
   protocolParamsUtxo: UTxO;
-  proposedProtocolParamsDatum: ProtocolParamsDatum;
   protocolProposalUtxo: UTxO;
-  protocolScriptUtxos: [UTxO];
+  protocolScriptUtxos: UTxO[];
+  txTimePadding?: TimeDifference;
 };
 
-export function applyProposalTx(
+export function applyProtocolProposalTx(
   lucid: Lucid,
   {
-    // protocolParamsDatum,
-    // proposedProtocolParamsDatum,
     protocolParamsUtxo,
     protocolProposalUtxo,
     protocolScriptUtxos,
-  }: ApplyTxParams
+    txTimePadding = 20000,
+  }: ApplyProtocolTxParams
 ) {
-  const protocolGovernorPkh = ""; // FIXME:
+  assert(protocolParamsUtxo.datum, "Protocol params utxo must have datum");
+  const protocolParamsDatum: ProtocolParamsDatum = S.fromData(
+    S.fromCbor(protocolParamsUtxo.datum),
+    ProtocolParamsDatum
+  );
 
-  // TODO: @sk-umiuma: Implement this
-  const protocolParamsRedeemer: Redeemer = Data.empty();
-  // TODO: @sk-umiuma: Implement this
-  const protocolProposalRedeemer: Redeemer = Data.empty();
+  assert(protocolProposalUtxo.datum, "Protocol proposal utxo must have datum");
+  const protocolProposalDatum: ProtocolProposalDatum = S.fromData(
+    S.fromCbor(protocolProposalUtxo.datum),
+    ProtocolProposalDatum
+  );
+
+  assert(protocolProposalDatum.proposal, "Protocol proposal must not be empty");
+  const appliedProtocolParamsDatum = protocolProposalDatum.proposal.params;
+
+  assert(
+    protocolParamsDatum.governorAddress.paymentCredential.paymentType ===
+      "PubKey",
+    "Governor address must have a public-key hash credential"
+  );
+  const protocolGovernorPkh = toHex(
+    protocolParamsDatum.governorAddress.paymentCredential.$.pubKeyHash.$hash
+  );
 
   return lucid
     .newTx()
-    .addSigner(protocolGovernorPkh)
+    .addSignerKey(protocolGovernorPkh)
     .readFrom(protocolScriptUtxos)
-    .collectFrom([protocolParamsUtxo], protocolParamsRedeemer)
-    .collectFrom([protocolProposalUtxo], protocolProposalRedeemer)
+    .collectFrom(
+      [protocolParamsUtxo],
+      S.toCbor(S.toData({ case: "ApplyProposal" }, ProtocolParamsRedeemer))
+    )
+    .collectFrom(
+      [protocolProposalUtxo],
+      S.toCbor(S.toData({ case: "Apply" }, ProtocolProposalRedeemer))
+    )
     .payToContract(
       protocolProposalUtxo.address,
-      { inline: Data.empty() },
+      { inline: S.toCbor(S.toData({ proposal: null }, ProtocolProposalDatum)) },
       protocolProposalUtxo.assets
     )
     .payToContract(
       protocolParamsUtxo.address,
-      { inline: Data.empty() }, // FIXME:
+      {
+        inline: S.toCbor(
+          S.toData(appliedProtocolParamsDatum, ProtocolParamsDatum)
+        ),
+      },
       protocolParamsUtxo.assets
-    );
+    )
+    .validFrom(Date.now() - txTimePadding);
 }
