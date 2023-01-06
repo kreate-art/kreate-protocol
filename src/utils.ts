0b8d66fff3f03ae3be490ea0c61ad1f2f5a3f393
Umiuma <umiuma@shinka.network>
2023-01-06 14:35:48 +0700

    fix: remove node:util

--- src/utils.ts
+++ src/utils.ts
@@ -1,12 +1,6 @@
-import * as util from "node:util";
-
 export function assert(
   condition: unknown,
   message?: string
 ): asserts condition {
   if (!condition) throw new Error(message || "assertion failed");
-}
-
-export function inspect(value: unknown, options?: util.InspectOptions): string {
-  return util.inspect(value, { depth: null, colors: true, ...options });
 }
