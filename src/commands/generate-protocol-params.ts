61ab871a5ca3fb9acc6f06a906f2cd9b99d44168
sk-saru <saru@shinka.network>
2023-01-14 18:54:10 +0700

    feat: update non-script protocol params

--- src/commands/generate-protocol-params.ts
+++ src/commands/generate-protocol-params.ts
@@ -174,21 +174,45 @@ export function getProtocolRegistry(
 
 // TODO: sk-saru should read from config file
 export const SAMPLE_PROTOCOL_NON_SCRIPT_PARAMS: ProtocolNonScriptParams = {
+  // The Protocol Governor’s share of the protocol revenue (ADA & Teiki).
+  // The ratio is represented by a [0..1] number multiplied by 1E+6.
   governorShareRatio: 800_000n, // 80%
-  protocolFundsShareRatio: 100_000n, // 10%
+  // The Protocol’s share (including Protocol Governor’s) share of the raised funds.
+  // The ratio is represented by a [0..1] number multiplied by 1E+6.
+  protocolFundsShareRatio: 45_000n, // 4.5%
+  // The price per discount cent in Lovelace. Should be around 1E+4, to resemble a cent of an ADA. See Actors & Discounts.
   discountCentPrice: 10_000n,
-  projectMilestones: [1_000_000_000n, 5_000_000_000n, 10_000_000_000n],
+  // Projects funding milestones in Lovelace.
+  // Note that we can have a more compact format.
+  projectMilestones: [
+    1_500_000_000n,
+    3_000_000_000n,
+    6_000_000_000n,
+    12_000_000_000n,
+    25_000_000_000n,
+    100_000_000_000n,
+  ],
+  // The r protocol parameter in @Teiki Tokenomics.
   teikiCoefficient: 500n,
+  // The burn rate of projects' Teiki per epoch.
+  // The rate is represented by a [0..1] number multiplied by 1E+6.
   projectTeikiBurnRate: 50_000n, // 5% per epoch
-  epochLength: { milliseconds: 200_000n }, // 10 blocks / epoch
-  projectPledge: 100_000_000n,
+  // The length of an epoch in milliseconds - Cardano Protocol Params.
+  epochLength: { milliseconds: 86_400_000n }, // 1 day / epoch -- preview
+  projectPledge: 500_000_000n,
   projectCreationFee: 10_000_000n,
-  projectSponsorshipFee: 50_000_000n,
-  projectSponsorshipDuration: { milliseconds: 86_400_000n }, // 1 day
-  projectInformationUpdateFee: 4_000_000n,
-  projectCommunityUpdateFee: 2_000_000n,
+  // Minimum project sponsorship fee ADA (in Lovelace). Higher number, higher position.
+  projectSponsorshipFee: 3_000_000_000n,
+  // If the Project Owner paid the sponsorship fee, the project would be sponsored for this duration (in milliseconds).
+  projectSponsorshipDuration: { milliseconds: 864_000_000n }, // 10 days
+  projectInformationUpdateFee: 1_000_000n,
+  projectCommunityUpdateFee: 500_000n,
+  //The minimum number of shared treasuries need to be created (to receive Teiki) per milestone-reached event.
   minTreasuryPerMilestoneEvent: 10n,
+  // The amount of ADA (in Lovelace) of a stake registration deposit - Cardano Protocol Params.
   stakeKeyDeposit: 2_000_000n,
+  // Proposals have to go through a waiting period.
   proposalWaitingPeriod: { milliseconds: 20_000n }, // 1 block
+  // Project delistings have to go through a waiting period. Initiated by Protocol Governor. Can be canceled during this period.
   projectDelistWaitingPeriod: { milliseconds: 20_000n }, // 1 block
 };
