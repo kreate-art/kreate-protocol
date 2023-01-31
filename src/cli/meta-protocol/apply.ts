28acd61f5cdebd698ebcdb5459cd2f3ec1e6dfc2
Umiuma <umiuma@shinka.network>
2023-01-31 16:26:28 +0700

    feat: meta-protocol apply & propose cli

---
+++ src/cli/meta-protocol/apply.ts
@@ -0,0 +1,31 @@
+import { Unit } from "lucid-cardano";
+
+import { getLucid } from "@/commands/utils";
+import { TEIKI_PLANT_NFT_TOKEN_NAME } from "@/contracts/common/constants";
+import { signAndSubmit } from "@/helpers/lucid";
+import { applyMetaProtocolProposalTx } from "@/transactions/meta-protocol/apply";
+
+const lucid = await getLucid();
+const teikiPlantNftMph = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const teikiPlantNftUnit: Unit = teikiPlantNftMph + TEIKI_PLANT_NFT_TOKEN_NAME;
+const teikiPlantAddress = "addr1xxxx";
+
+const teikiPlantUtxo = (
+  await lucid.utxosAtWithUnit(teikiPlantAddress, teikiPlantNftUnit)
+)[0];
+
+const teikiPlantScriptUtxo = (
+  await lucid.utxosByOutRef([{ txHash: "", outputIndex: 0 }])
+)[0];
+
+const tx = applyMetaProtocolProposalTx(lucid, {
+  teikiPlantUtxo,
+  teikiPlantScriptUtxo,
+});
+
+const txComplete = await tx.complete();
+const txHash = await signAndSubmit(txComplete);
+
+await lucid.awaitTx(txHash);
+
+console.log("txHash :>> ", txHash);
---
+++ src/cli/meta-protocol/propose.ts
@@ -0,0 +1,59 @@
+import { Unit } from "lucid-cardano";
+
+import { getLucid } from "@/commands/utils";
+import { TEIKI_PLANT_NFT_TOKEN_NAME } from "@/contracts/common/constants";
+import { signAndSubmit } from "@/helpers/lucid";
+import * as S from "@/schema";
+import { RulesProposal, TeikiPlantDatum } from "@/schema/teiki/meta-protocol";
+import { proposeMetaProtocolProposalTx } from "@/transactions/meta-protocol/propose";
+import { assert } from "@/utils";
+
+const lucid = await getLucid();
+const teikiPlantNftMph = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const teikiMph = "xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx";
+const teikiPlantNftUnit: Unit = teikiPlantNftMph + TEIKI_PLANT_NFT_TOKEN_NAME;
+const teikiPlantAddress = "addr1xxxx";
+
+const teikiPlantUtxo = (
+  await lucid.utxosAtWithUnit(teikiPlantAddress, teikiPlantNftUnit)
+)[0];
+
+const teikiPlantScriptUtxo = (
+  await lucid.utxosByOutRef([{ txHash: "", outputIndex: 0 }])
+)[0];
+
+assert(
+  teikiPlantUtxo.datum != null,
+  "Invalid Teiki plant UTxO: Missing inline datum"
+);
+const teikiPlantDatum = S.fromData(
+  S.fromCbor(teikiPlantUtxo.datum),
+  TeikiPlantDatum
+);
+
+const proposedRules: RulesProposal = {
+  inEffectAt: { timestamp: BigInt(Date.now() + 60000) },
+  rules: {
+    ...teikiPlantDatum.rules,
+    teikiMintingRules: [
+      {
+        mintingPolicyHash: { script: { hash: teikiMph } },
+        redeemer: { kind: "ConstrNotIn", constrs: [2n] },
+        amounts: null,
+      },
+    ],
+  },
+};
+
+const tx = proposeMetaProtocolProposalTx(lucid, {
+  teikiPlantUtxo,
+  teikiPlantScriptUtxo,
+  proposedRules,
+});
+
+const txComplete = await tx.complete();
+const txHash = await signAndSubmit(txComplete);
+
+await lucid.awaitTx(txHash);
+
+console.log("txHash :>> ", txHash);
