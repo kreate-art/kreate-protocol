62a42bd7f46cc006251118545adc470d47363374
sk-saru <saru@shinka.network>
2023-02-07 15:32:02 +0700

    feat: add reclaim script utxos

--- src/transactions/protocol/reclaim-scripts.ts
+++ src/transactions/protocol/reclaim-scripts.ts
@@ -1,24 +1,33 @@
 import { Data, Lucid, UTxO } from "lucid-cardano";
 
+import { extractPaymentPubKeyHash } from "@/helpers/schema";
+import * as S from "@/schema";
 import { ProtocolParamsDatum } from "@/schema/teiki/protocol";
+import { assert } from "@/utils";
 
-export type ReclaimProtocolScriptParams = {
-  protocolParamsDatum: ProtocolParamsDatum;
+export type Params = {
+  protocolParamsUtxo: UTxO;
   reclaimUtxos: UTxO[];
+  protocolScriptVRefScriptUtxo: UTxO;
 };
 
-// TODO: @sk-umiuma: Add the commented params
 export function reclaimProtocolScriptTx(
   lucid: Lucid,
-  {
-    // protocolParamsDatum,
-    reclaimUtxos,
-  }: ReclaimProtocolScriptParams
+  { protocolParamsUtxo, reclaimUtxos, protocolScriptVRefScriptUtxo }: Params
 ) {
-  const protocolGovernorPkh = ""; // FIXME:
+  assert(protocolParamsUtxo.datum, "Protocol params utxo must have datum");
+  const protocolParams = S.fromData(
+    S.fromCbor(protocolParamsUtxo.datum),
+    ProtocolParamsDatum
+  );
+
+  const protocolGovernorPkh = extractPaymentPubKeyHash(
+    protocolParams.governorAddress
+  );
 
   return lucid
     .newTx()
+    .readFrom([protocolParamsUtxo, protocolScriptVRefScriptUtxo])
     .addSignerKey(protocolGovernorPkh)
     .collectFrom(reclaimUtxos, Data.void());
 }
