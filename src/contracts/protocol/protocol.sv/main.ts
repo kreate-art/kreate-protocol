73bb79cba842dd5f438c9e6ce741aca6bf1e6066
Shishi | Shinka <shishi@shinka.network>
2023-02-17 15:44:25 +0700

    refine: SV | Protocol

--- src/contracts/protocol/protocol.sv/main.ts
+++ src/contracts/protocol/protocol.sv/main.ts
@@ -12,17 +12,20 @@ export default function main({ protocolNftMph }: Params): HeliosScript {
 
     import { RATIO_MULTIPLIER }
       from ${module("constants")}
-    import { Datum as OpenTreasuryDatum }
-      from ${module("v__open_treasury__types")}
-    import { Datum as PParamsDatum }
-      from ${module("v__protocol_params__types")}
 
     import {
+      is_tx_authorized_by,
       find_pparams_datum_from_inputs,
       find_pparams_datum_from_outputs,
       stakingCredentialToSVH,
-      is_tx_authorized_by
+      scriptHashToStakingCredential
     } from ${module("helpers")}
+
+    import { Datum as OpenTreasuryDatum }
+      from ${module("v__open_treasury__types")}
+
+    import { Datum as PParamsDatum }
+      from ${module("v__protocol_params__types")}
 
     const PROTOCOL_NFT_MPH: MintingPolicyHash =
       MintingPolicyHash::new(#${protocolNftMph})
@@ -31,53 +34,59 @@ export default function main({ protocolNftMph }: Params): HeliosScript {
       tx: Tx = ctx.tx;
 
       ctx.get_script_purpose().switch {
+
         rewarding: Rewarding => {
           pparams_datum: PParamsDatum =
             find_pparams_datum_from_inputs(tx.ref_inputs, PROTOCOL_NFT_MPH);
 
           own_staking_credential: StakingCredential = rewarding.credential;
 
-          withdraw_amount: Int = tx.withdrawals.get(own_staking_credential);
+          withdrawn_amount: Int = tx.withdrawals.get(own_staking_credential);
+
+          governor_share_amount: Int =
+            withdrawn_amount * pparams_datum.governor_share_ratio / RATIO_MULTIPLIER;
 
           open_treasury_address: Address = Address::new(
             Credential::new_validator(
               pparams_datum.registry.open_treasury_validator.latest
             ),
-            Option[StakingCredential]::Some{own_staking_credential}
+            Option[StakingCredential]::Some {
+              scriptHashToStakingCredential(
+                pparams_datum.registry.protocol_staking_validator
+              )
+            }
           );
 
-          open_treasury_txout: TxOutput =
+          open_treasury_output: TxOutput =
             tx.outputs.find(
-              (output: TxOutput) -> Bool {
+              (output: TxOutput) -> {
                 output.address == open_treasury_address
                   && output.value.to_map().length == 1
-                  && output.value >= Value::lovelace(withdraw_amount)
+                  && output.value.get(AssetClass::ADA) >= withdrawn_amount
               }
             );
 
           open_treasury_datum: OpenTreasuryDatum =
-            open_treasury_txout.datum
+            open_treasury_output.datum
               .switch {
                 i: Inline => OpenTreasuryDatum::from_data(i.data),
                 else => error("Invalid open treasury utxo: must inline datum")
               };
 
-          own_staking_validator_hash: StakingValidatorHash =
-            stakingCredentialToSVH(own_staking_credential);
-
-          open_treasury_datum.governor_ada == withdraw_amount * pparams_datum.governor_share_ratio / RATIO_MULTIPLIER
+          open_treasury_datum.governor_ada == governor_share_amount
             && open_treasury_datum.tag.switch {
-              tag: TagProtocolStakingRewards => tag.staking_validator == own_staking_validator_hash,
+              tag: TagProtocolStakingRewards => {
+                tag.staking_validator == stakingCredentialToSVH(own_staking_credential)
+              },
               else => false
             }
         },
+
         certifying: Certifying => {
-          //TODO: optimize this
           pparams_datum: PParamsDatum =
             find_pparams_datum_from_outputs(
-              tx.ref_inputs.map(
-                (input: TxInput) -> TxOutput {input.output}
-              ) + tx.outputs,
+              tx.ref_inputs.map((input: TxInput) -> { input.output })
+                + tx.outputs,
               PROTOCOL_NFT_MPH
             );
 
@@ -92,6 +101,7 @@ export default function main({ protocolNftMph }: Params): HeliosScript {
             else => false
           }
         },
+
         else => false
       }
     }
