3edefee5d388d00028d344ee9dadacfa73f73339
Shishi | Shinka <shishi@shinka.network>
2023-02-17 14:37:21 +0700

    refine: NFT | Protocol

--- src/contracts/protocol/protocol.nft/main.ts
+++ src/contracts/protocol/protocol.nft/main.ts
@@ -10,9 +10,6 @@ export default function main({ protocolSeed }: Params): HeliosScript {
   return helios`
     ${header("minting", "nft__protocol")}
 
-    import { Redeemer }
-      from ${module("nft__protocol__types")}
-
     import { does_consume_input_with_output_id }
       from ${module("helpers")}
 
@@ -21,7 +18,10 @@ export default function main({ protocolSeed }: Params): HeliosScript {
       PROTOCOL_PROPOSAL_NFT_TOKEN_NAME
     } from ${module("constants")}
 
-    const seed_output_id: TxOutputId =
+    import { Redeemer }
+      from ${module("nft__protocol__types")}
+
+    const SEED_OUTPUT_ID: TxOutputId =
       TxOutputId::new(
         TxId::new(#${protocolSeed.txHash}),
         ${protocolSeed.outputIndex}
@@ -32,10 +32,11 @@ export default function main({ protocolSeed }: Params): HeliosScript {
       own_mph: MintingPolicyHash = ctx.get_current_minting_policy_hash();
 
       redeemer.switch {
+
         Bootstrap => {
           assert (
             tx.minted.to_map().get(own_mph).all(
-              (token_name: ByteArray, amount: Int) -> Bool {
+              (token_name: ByteArray, amount: Int) -> {
                 if (token_name == PROTOCOL_PARAMS_NFT_TOKEN_NAME) { amount == 1 }
                 else if (token_name == PROTOCOL_PROPOSAL_NFT_TOKEN_NAME) { amount == 1 }
                 else { false }
@@ -44,9 +45,9 @@ export default function main({ protocolSeed }: Params): HeliosScript {
             "Transaction must mint only two protocol params nft and protocol proposal nft"
           );
 
-          does_consume_input_with_output_id(tx, seed_output_id)
-        },
-        else => false
+          does_consume_input_with_output_id(tx, SEED_OUTPUT_ID)
+        }
+
       }
     }
   `;
