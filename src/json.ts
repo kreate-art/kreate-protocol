f7e901d70f0eb302fff2c9c826c687616d879af1
Shishi | Shinka <shishi@shinka.network>
2023-02-19 23:03:59 +0700

    feat: synchronize json utils with other repos

--- src/json.ts
+++ src/json.ts
@@ -1,6 +1,5 @@
 // TODO: A duplication of teiki-web/modules/json-utils/index.ts
 import JsonBigFactory from "@shinka-network/json-bigint";
-
 const JsonBig = JsonBigFactory({ useNativeBigInt: true });
 
 export default JsonBig;
@@ -22,6 +21,9 @@ export function fromJson<T = unknown>(
     : JsonBig.parse(text);
 }
 
+type ValidJsonTypes = string | number | bigint | boolean | object | unknown[];
+type InvalidJsonTypes = undefined | symbol | ((...args: unknown[]) => unknown);
+
 /**
  * Converts a JS value to a JSON-formatted string.
  *
@@ -29,13 +31,26 @@ export function fromJson<T = unknown>(
  * This function is the bigint-aware alternative of `JSON.stringify`.
  */
 export function toJson(
-  value: string | number | bigint | boolean | object | unknown[],
-  space?: number
+  value: InvalidJsonTypes,
+  replacer?: Replacer,
+  space?: string | number
+): undefined;
+export function toJson(
+  value: ValidJsonTypes,
+  replacer?: Replacer,
+  space?: string | number
 ): string;
 export function toJson(
-  value: undefined | symbol | ((...args: unknown[]) => unknown)
-): undefined;
-export function toJson(value: unknown, space?: number): string | undefined {
-  // JSON.stringify has bad typing
-  return JsonBig.stringify(value, null, space);
+  value: unknown,
+  replacer?: Replacer,
+  space?: string | number
+): string | undefined;
+export function toJson(
+  value: unknown,
+  replacer?: Replacer,
+  space?: string | number
+): string | undefined {
+  return JsonBig.stringify(value, replacer, space);
 }
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+type Replacer = (this: any, key: string, value: any) => any;
