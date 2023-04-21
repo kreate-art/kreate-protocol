fe6e7ea286fa934c164ca1a24072157aba5c0035
Shinka Tori <hai@kreate.community>
2023-04-21 12:26:14 +0700

    Revert "build: finally bump lucid"
    This reverts commit af4299bed47dcaf8e12e721acd2013229290d429.

--- src/helpers/lucid.ts
+++ src/helpers/lucid.ts
@@ -5,7 +5,7 @@ import {
   AddressDetails,
   getAddressDetails,
   TxComplete,
-  C,
+  Core,
   fromHex,
 } from "lucid-cardano";
 
@@ -71,7 +71,7 @@ export function extractWitnessKeyHashes({
   witnesses,
 }: {
   txId: Hex;
-  witnesses: C.TransactionWitnessSet;
+  witnesses: Core.TransactionWitnessSet;
 }) {
   const vkeys = witnesses.vkeys();
   if (!vkeys) return [];
